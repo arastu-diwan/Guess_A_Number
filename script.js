8115d240d647d55d23741af1aae9005e7d27bfd8
Arastu Diwan <55242477+arastu-diwan@users.noreply.github.com>
2024-11-04 21:10:51 +0530

    Add files via upload
    Final commit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+// document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+let highScore = 0;
+
+const getMessage = (message)=>{
+document.querySelector('.message').textContent = message;
+}
+
+//random generates number btwn 0 and 1, we multiply that by 20 to get range 0 to 19(20 is not included), to include +1
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value); // userInput in guess text box.
+
+    //every text innput is string type in js, we need to convert to number
+    if(!guess){
+        getMessage('Please enter number to continue!')
+    }else if(score > 0){
+        if(secretNumber !== guess){
+            getMessage(secretNumber < guess ? 'Too High!' : 'Too low!');
+            score--;
+            document.querySelector('.score').textContent = score; 
+        }else if(secretNumber === guess){
+            getMessage('Correct Answer🤩');
+            score++;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.number').textContent = secretNumber;
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            if(score > highScore){
+            highScore =  score;
+            document.querySelector('.highscore').textContent = highScore;
+            }
+           }
+    } else {
+        getMessage('You lost the Game!!');
+    }
+
+
+})
+
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    getMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222'; 
+})
